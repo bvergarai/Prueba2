70cef1a631f0cb77a5e7b10698aad1fadfb27df6
Benjamin Vergara <benjasnow19@gmail.com>
2018-11-27 16:24:15 -0300

    intentando arreglar el sw x3

--- Mis-Perris/perris/templates/perris/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-  // TODO add service worker code here
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
-
